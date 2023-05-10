652caf7aa873e09bb3de17786bfb7aacf0c6a306
MagicSalt <darrenlin815@hotmail.com>
2023-05-09 18:02:37 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+    apiKey: 'AIzaSyB-t8CpOhKnJ0MAJtaut61cf3OwMBLaWCQ',
+    authDomain: 'invoicing-1a197.firebaseapp.com',
+    projectId: 'invoicing-1a197',
+    storageBucket: 'invoicing-1a197.appspot.com',
+    messagingSenderId: '341482295174',
+    appId: '1:341482295174:web:78b6894ef2f6e8514e2607',
+    measurementId: 'G-BBME74N0V1',
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analystics = firebase.analytics();
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user] = useAuthState(auth);
+
+    return (
+        <div className='App'>
+            <header className='App-header'></header>
+            {user ? <Invoices /> : <SignIn />}
+        </div>
+    );
 }
 
 export default App;
